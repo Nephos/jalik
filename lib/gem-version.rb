849e6183671b46f6480cb88e811ba2a070f5e2f5
Arthur Poulet <arthur.poulet@cryptolab.net>
2014-10-28 11:53:36 +0100

    up to 1.1 to finish

--- lib/gem-version.rb
+++ lib/gem-version.rb
@@ -1,5 +1,5 @@
 module Jalik
 
-  VERSION = '1.0'
+  VERSION = '1.1'
 
 end
--- lib/jalik/parser.rb
+++ lib/jalik/parser.rb
@@ -3,9 +3,21 @@
 module Jalik
 
   # @param jalik_value [String] a jalik link like "jalik('details[!]ancien[*]presbytere[*][#][;]');"
+  # @param proto [String] "http://" or "https://"
+  # @param host [String] site address (domain or ip)
   # @return [String]
-  def self.parse(jalik_value)
-    return jalik_value.to_s.gsub("jalik('", "").gsub("');", "").gsub("[*]", "+").gsub("[#]", ".").gsub("[!]", "-").gsub("[;]", "html")
+  def self.parse(jalik_value, proto="http://", host="127.0.0.1")
+    jalik_value = jalik_value.to_s
+    jalik_value.gsub!(/jalik\('(.*)'\);/, '\1')
+    jalik_value.gsub!('[x]', proto.to_s + host.to_s);
+    jalik_value.gsub!('[h]', host.to_s);
+    jalik_value.gsub!('[|]','/');
+    jalik_value.gsub!('[#]', '.');
+    jalik_value.gsub!('[!]', '-');
+    jalik_value.gsub!('[*]','+');
+    jalik_value.gsub!('[;]', 'html');
+    jalik_value.gsub!('[@]', 'php');
+    return jalik_value
   end
 
 end
